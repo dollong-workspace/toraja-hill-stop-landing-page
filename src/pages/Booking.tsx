466403591ba831e3e7084e8926fda0530c9f1cb8
doltonsedward <edwarddoltons@gmail.com>
2026-01-23 09:27:35 +0700

    fix spacing

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -204,7 +204,7 @@ Apakah kamar tersedia?`;
           </div>
 
           {/* Main Content */}
-          <div className="container mx-auto px-4 lg:px-8 py-10 lg:py-16">
+          <div className="container mx-auto px-4 lg:px-8">
             <div className="grid lg:grid-cols-3 gap-8 lg:gap-12">
               {/* Left Column - Booking Form */}
               <div className="lg:col-span-2">
